29b78a82c37be8871ac97a92892b1dd352131407
Chaitanya Dodiya <chaitanya_dodiya>
2020-05-14 21:30:57 +0530

    Able to read all the lines of files

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 def main():
 
-	pass
+	o = 0
+	link = 0
+	a = 0	
+	with open("./assets/Chat_backup.txt", 'r', encoding = 'utf-8') as f:
+		for i in f.readlines():
+			if 'omitted' in i:
+				print(i)
+				o += 1
+			elif 'http://' in i:
+				print(i)
+				link += 1
+			else:
+				a += 1
+
+	print(f'omitted {o}')
+	print(f'links {link}')
+	print(f'{a}')
 
 
 if __name__ == '__main__':
